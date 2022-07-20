c7dc029cb5c67d8f548fd87ef35e17ae9fd7fd5d
tempiltin <temurbekshukurov0707@gmail.com>
2022-07-20 12:15:01 +0500

    upload

--- src/Components/NavigationBottom/Bottom1.jsx
+++ src/Components/NavigationBottom/Bottom1.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {BsBasket} from "react-icons/bs"
+
 const Bottom1 = () => {
   return (
    
-      <ul className='bottomClass'>
-        <li><a href="#!"><BsBasket/><span>0</span> </a></li>
-      </ul>
+      <button className='bottomClass'>
+        korzina
+      </button>
   
   )
 }
