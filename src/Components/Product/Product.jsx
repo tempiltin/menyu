ac787d6a976f60675c8090305280707c67580078
tempiltin <temurbekshukurov0707@gmail.com>
2022-07-21 06:52:07 +0500

    upload

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,5 +1,5 @@
 
-import React from "react";
+import React, { useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -10,40 +10,52 @@ import img from "../../images/food-sutherland-foodservice-12.png"
 const Product = () => {
 
     let dev = 98787987887875465454654657898787998797978
- const idRandom =   Math.floor((Math.random() * dev) + 1);
+    const idRandom = Math.floor((Math.random() * dev) + 1);
     const Data = [
         {
-            name:"shurva",
-            img:img,
-            price:30000,
-            id:idRandom,
+            name: "shurva",
+            img: img,
+            price: 30000,
+            id: idRandom,
         },
         {
-            name:"shurva",
-            img:img,
-            price:30000,
-            id:idRandom,
+            name: "shurva",
+            img: img,
+            price: 30000,
+            id: idRandom,
         },
         {
-            name:"shurva",
-            img:img,
-            price:30000,
-            id:idRandom,
+            name: "shurva",
+            img: img,
+            price: 30000,
+            id: idRandom,
         },
         {
-            name:"shurva",
-            img:img,
-            price:30000,
-            id:idRandom,
+            name: "shurva",
+            img: img,
+            price: 30000,
+            id: idRandom,
         },
         {
-            name:"shurva",
-            img:img,
-            price:30000,
-            id:idRandom,
+            name: "shurva",
+            img: img,
+            price: 30000,
+            id: idRandom,
         },
     ]
     console.log(Data);
+
+
+    const [count, setCount] = useState(0);
+
+
+    const handleIncrement = () => {
+        setCount(count + 1)
+    }
+    const handleDecrement = () => {
+        setCount(count - 1)
+    }
+
     return (
         <div className='container'>
             <h1>Food Menu</h1>
@@ -62,226 +74,33 @@ const Product = () => {
                                 className="mySwiper"
                             >
                                 {
-                                    Data.map(({img , name , price ,id},index) => (
+                                    Data.map(({ img, name, price, id }, index) => (
 
-                                        <SwiperSlide key={index}>
+                                        <SwiperSlide className="apiclass" key={index}>
+                                            <span className={`${count !== 0 ? "card__badge" : "card__badge--hidden"
+                                                }`}  >{count}</span>
                                             <img src={img} alt="logo" />
-                                             <h3>{name}</h3>
-                                             <h4>{price}</h4>
-                                             <div className="butdiv">
-                                             <button>+</button>
-                                             <button>-</button>
-                                             </div>
+                                            <div className="d-flex ">
+                                            <h3>{name}</h3>
+                                            <h3 className="h4">{price}</h3>
+                                            </div>
+                                            <div className="butdiv">
+                                                <button className="btn" onClick={handleIncrement}>+</button>
+                                                {count !== 0 ? (
+                                                    <button className="btn" onClick={handleDecrement}>-</button>
+                                                ) : (
+                                                    ""
+                                                )}
+
+                                            </div>
                                         </SwiperSlide>
                                     ))
                                 }
 
                             </Swiper>
                         </div>
-                     
-                        <div className="category mt-5">
-                            <h2>Yengil Taomlar</h2>
-                            <Swiper
-                                slidesPerView={2}
-                                spaceBetween={30}
-                                pagination={{
-                                    clickable: true,
-                                }}
 
-                                className="mySwiper"
-                            >
-                                {
-                                    Data.map(({img , name , price ,id},index) => (
 
-                                        <SwiperSlide key={index}>
-                                            <img src={img} alt="logo" />
-                                             <h3>{name}</h3>
-                                             <h4>{price}</h4>
-                                             <div className="butdiv">
-                                             <button>+</button>
-                                             <button>-</button>
-                                             </div>
-                                        </SwiperSlide>
-                                    ))
-                                }
-
-                            </Swiper>
-                        </div>
-                     
-                        <div className="category mt-5" >
-                            <h2>Yengil Taomlar</h2>
-                            <Swiper
-                                slidesPerView={2}
-                                spaceBetween={30}
-                                pagination={{
-                                    clickable: true,
-                                }}
-
-                                className="mySwiper"
-                            >
-                                {
-                                    Data.map(({img , name , price ,id},index) => (
-
-                                        <SwiperSlide key={index}>
-                                            <img src={img} alt="logo" />
-                                             <h3>{name}</h3>
-                                             <h4>{price}</h4>
-                                             <div className="butdiv">
-                                             <button>+</button>
-                                             <button>-</button>
-                                             </div>
-                                        </SwiperSlide>
-                                    ))
-                                }
-
-                            </Swiper>
-                        </div>
-                     
-                        <div className="category mt-5">
-                            <h2>Yengil Taomlar</h2>
-                            <Swiper
-                                slidesPerView={2}
-                                spaceBetween={30}
-                                pagination={{
-                                    clickable: true,
-                                }}
-
-                                className="mySwiper"
-                            >
-                                {
-                                    Data.map(({img , name , price ,id},index) => (
-
-                                        <SwiperSlide key={index}>
-                                            <img src={img} alt="logo" />
-                                             <h3>{name}</h3>
-                                             <h4>{price}</h4>
-                                             <div className="butdiv">
-                                             <button>+</button>
-                                             <button>-</button>
-                                             </div>
-                                        </SwiperSlide>
-                                    ))
-                                }
-
-                            </Swiper>
-                        </div>
-                     
-                        <div className="category mt-5">
-                            <h2>Yengil Taomlar</h2>
-                            <Swiper
-                                slidesPerView={2}
-                                spaceBetween={30}
-                                pagination={{
-                                    clickable: true,
-                                }}
-
-                                className="mySwiper"
-                            >
-                                {
-                                    Data.map(({img , name , price ,id},index) => (
-
-                                        <SwiperSlide key={index}>
-                                            <img src={img} alt="logo" />
-                                             <h3>{name}</h3>
-                                             <h4>{price}</h4>
-                                             <div className="butdiv">
-                                             <button>+</button>
-                                             <button>-</button>
-                                             </div>
-                                        </SwiperSlide>
-                                    ))
-                                }
-
-                            </Swiper>
-                        </div>
-                     
-                        <div className="category mt-5">
-                            <h2>Yengil Taomlar</h2>
-                            <Swiper
-                                slidesPerView={2}
-                                spaceBetween={30}
-                                pagination={{
-                                    clickable: true,
-                                }}
-
-                                className="mySwiper"
-                            >
-                                {
-                                    Data.map(({img , name , price ,id},index) => (
-
-                                        <SwiperSlide key={index}>
-                                            <img src={img} alt="logo" />
-                                             <h3>{name}</h3>
-                                             <h4>{price}</h4>
-                                             <div className="butdiv">
-                                             <button>+</button>
-                                             <button>-</button>
-                                             </div>
-                                        </SwiperSlide>
-                                    ))
-                                }
-
-                            </Swiper>
-                        </div>
-                     
-                        <div className="category mt-5">
-                            <h2>Yengil Taomlar</h2>
-                            <Swiper
-                                slidesPerView={2}
-                                spaceBetween={30}
-                                pagination={{
-                                    clickable: true,
-                                }}
-
-                                className="mySwiper"
-                            >
-                                {
-                                    Data.map(({img , name , price ,id},index) => (
-
-                                        <SwiperSlide key={index}>
-                                            <img src={img} alt="logo" />
-                                             <h3>{name}</h3>
-                                             <h4>{price}</h4>
-                                             <div className="butdiv">
-                                             <button>+</button>
-                                             <button>-</button>
-                                             </div>
-                                        </SwiperSlide>
-                                    ))
-                                }
-
-                            </Swiper>
-                        </div>
-                     
-                        <div className="category mt-5">
-                            <h2>Yengil Taomlar</h2>
-                            <Swiper
-                                slidesPerView={2}
-                                spaceBetween={30}
-                                pagination={{
-                                    clickable: true,
-                                }}
-
-                                className="mySwiper"
-                            >
-                                {
-                                    Data.map(({img , name , price ,id},index) => (
-
-                                        <SwiperSlide key={index}>
-                                            <img src={img} alt="logo" />
-                                             <h3>{name}</h3>
-                                             <h4>{price}</h4>
-                                             <div className="butdiv">
-                                             <button>+</button>
-                                             <button>-</button>
-                                             </div>
-                                        </SwiperSlide>
-                                    ))
-                                }
-
-                            </Swiper>
-                        </div>
-                     
                     </div>
                 </div>
             </div>
